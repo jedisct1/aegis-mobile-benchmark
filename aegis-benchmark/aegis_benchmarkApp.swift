ebe338f92ac9c14ffc34270a21d3121acb2f8524
Frank Denis <github@pureftpd.org>
2024-10-21 19:42:49 +0200

    Initial Commit

---
+++ aegis-benchmark/aegis_benchmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  aegis_benchmarkApp.swift
+//  aegis-benchmark
+//
+//  Created by Frank Denis on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct aegis_benchmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
